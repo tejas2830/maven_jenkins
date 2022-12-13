555ee2337edf91ca91a3428d96478cb05d03f0b8
tejas <tejas@gmail.com>
2022-12-13 20:16:23 +0530

    1st commit

---
+++ src/test/java/com/cadilac/lyriq/LyriqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadilac.lyriq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyriqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
